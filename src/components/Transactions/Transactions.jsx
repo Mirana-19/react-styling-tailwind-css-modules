9ef9b0ca065261f0727fe37879e929743a225f72
Mirana-19 <martysik19@gmail.com>
2025-01-30 23:01:23 +0100

    adds styles

--- src/components/Transactions/Transactions.jsx
+++ src/components/Transactions/Transactions.jsx
@@ -2,19 +2,19 @@
 
 export const Transactions = ({ transactions }) => {
   return (
-    <table className="mx-auto mt-[50px]">
+    <table className="mx-auto mt-[50px] w-[450px]">
       <thead>
         <tr className="bg-blue-300">
-          <th className="border border-gray-300 px-4 py-2">Type</th>
-          <th className="border border-gray-300 px-4 py-2">Amount</th>
-          <th className="border border-gray-300 px-4 py-2">Currency</th>
+          <th className="border border-gray-300 px-4 py-2 uppercase">Type</th>
+          <th className="border border-gray-300 px-4 py-2 uppercase">Amount</th>
+          <th className="border border-gray-300 px-4 py-2 uppercase">Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {transactions.map(({ id, type, amount, currency }) => (
-          <tr key={id} className="even:bg-gray-100">
-            <td className="border border-gray-300 px-4 py-2">{type}</td>
+          <tr key={id} className="even:bg-gray-100 text-center">
+            <td className="border border-gray-300 px-4 py-2 capitalize">{type}</td>
             <td className="border border-gray-300 px-4 py-2">{amount}</td>
             <td className="border border-gray-300 px-4 py-2">{currency}</td>
           </tr>
